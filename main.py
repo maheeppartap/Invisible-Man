a38886025612eb0cb44355bb74ecd90144059809
JacobGoreski <jacob.goreski@gmail.com>
2021-02-13 17:39:03 -0800

    facedetectionv0.1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk
 
+from filehandler import facedetection
 from openCVHandler import OpenCVHandler
 from src import *
 
@@ -13,7 +14,8 @@ if __name__ == '__main__':
     opencv = OpenCVHandler(sys.argv[1], sys.argv[2])
 
 
-
+image1 = open('test_pictures/handsome_guy.jpg', 'r+b')
+facedetection(image1)
 
 
 
