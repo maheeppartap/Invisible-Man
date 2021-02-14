d22d3d8863f6cb447a9702670c5ec4d359282bb0
Kimia <knafar@sfu.ca>
2021-02-13 23:31:32 -0800

    stupid stuff

--- src/tkinterHandler.py
+++ src/tkinterHandler.py
@@ -1,4 +1,18 @@
+from tkinter import *
 
-class UIHandler:
-    def __init__(self):
-        print ("UI initialized")
+#class UIHandler:
+#    def __init__(self):
+#        print ("UI initialized")
+
+def clicked():
+    label.configure(text="you clicked, yay")
+
+window = Tk()
+window.title("Invisible Man")
+label = Label(window, text="Invisible Man", font=("Times New Roman", 30))
+label.grid(column=0, row=0)
+window.geometry("350x200")
+bt = Button(window, text="Enter", font=("Arial Bold", 25), command=clicked)
+bt.grid(column=1, row= 0)
+window.mainloop()
+
